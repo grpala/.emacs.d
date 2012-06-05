89380c32e33e5f54c7bccd619af2bd6ce4f24b41
Arm <arm@tortugasoft.com>
2012-06-05 15:26:37 -0300

    Initial commit

---
+++ conf/00annoyances.el
@@ -0,0 +1,41 @@
+;; C-h is backspace
+(keyboard-translate ?\C-h ?\C-?)
+
+;; Inhibir mensaje inicial
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-buffer-menu t)
+
+;; *scratch* limpio
+(setq initial-scratch-message nil)
+
+;; Wrapping
+(setq word-wrap t)
+
+;; Use legacy-style regions
+(setq transient-mark-mode nil)
+
+;; Use X clipboard for any copy-paste related action
+(setq x-select-enable-clipboard t)
+
+;; Add a new-line at the end of every file when saving
+(setq-default require-final-newline t)
+
+;; Remove every trailing whitespace when saving
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Remove every trailing blank line when saving
+(add-hook 'before-save-hook 'delete-blank-lines)
+
+;; Disable overwrite-mode
+(put 'overwrite-mode 'disabled t)
+
+;; Disable windows-like crap
+(setq tool-bar-mode nil)
+(setq menu-bar-mode nil)
+(setq scroll-bar-mode nil)
+
+;; Tabs
+(setq tab-width 3)
+(setq default-tab-width 3)
+(setq indent-tabs-mode nil)
---
+++ custom.el
@@ -0,0 +1,16 @@
+(print "loading custom.el...")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(ido-mode (quote both) nil (ido))
+ '(large-file-warning-threshold nil))
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
