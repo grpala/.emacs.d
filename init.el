e5212b2b2ad6b10b733ade2c81643fa1abbc5bb4
Guillermo R. Palavecino <grpala@gmail.com>
2019-06-24 18:12:58 -0300

    ignore emacs auto-backup .el files

--- init.el
+++ init.el
@@ -1,15 +1,18 @@
 ;; To use this modular conf, ~/.emacs must NOT exist
 
-(add-to-list 'load-path
-  (setq dotfiles-dir
-    (file-name-directory
-      (if load-file-name  ;; set to path of init.el
-         (file-truename load-file-name)
-         (buffer-file-name) ) ) ) )
+(setq dotfiles-dir
+  (file-name-directory
+    (if load-file-name  ;; set to path of init.el
+      (file-truename load-file-name)
+      (buffer-file-name) ) ) )
 
 (add-to-list 'load-path
-   (setq conf-dir (concat dotfiles-dir "conf")) )
+   (setq dist-dir (concat dotfiles-dir "dist/")) )
+
+(add-to-list 'load-path
+   (setq conf-dir (concat dotfiles-dir "conf/")) )
 
 ;; Setting custom-file is necessary, so customizations are saved there too.
 (load (setq custom-file (concat dotfiles-dir "custom.el")) 'noerror)
-(mapc #'load (directory-files conf-dir nil ".*\.el$"))
+(mapc #'load (directory-files conf-dir nil "^.[^#].*\.el$"))
+(put 'overwrite-mode 'disabled nil)
